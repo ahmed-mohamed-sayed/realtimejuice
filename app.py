925bf8f3c0ef13cec8d52d2c9bfe069183a54f82
ahmed-mohamed-sayed <82106817+ahmed-mohamed-sayed@users.noreply.github.com>
2022-07-16 08:03:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ st.markdown(f""" <style>
         padding-left: {padding}rem;
         padding-bottom: {padding}rem;
     }} </style> """, unsafe_allow_html=True)
-
+def local_style(file_name):
+        with open(file_name) as f:
+            st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+local_style(".streamlit/config.toml")
 
 
 #===============================================================================================
